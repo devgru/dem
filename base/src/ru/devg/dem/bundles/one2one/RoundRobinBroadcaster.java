728a6b65253f536a05db53727fb03b2cccf09440
Devgru <java@devg.ru>
2009-03-11 10:58:32 +0300

    RRB updated

--- base/src/ru/devg/dem/bundles/one2one/RoundRobinBroadcaster.java
+++ base/src/ru/devg/dem/bundles/one2one/RoundRobinBroadcaster.java
@@ -6,6 +6,7 @@ import ru.devg.dem.quanta.Handler;
 
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.List;
 
 /**
  * RoundRobinBroadcaster is a sophisticated broadcaster, that uses round-robin algorythm.
@@ -14,33 +15,28 @@ import java.util.ListIterator;
  * <li>sending event to empty RoundRobinBroadcaster will have no result;</li>
  * <li>when you add a Handler to RoundRobinBroadcaster it is added <i>before</i> the next Handler;</li>
  * </ul>
- * <p/>
- * <h2>Handling algorithm</h2>
- * For example, we have RoundRobinBroadcaster that contains
- * 3 {@link Handler handlers}, <code>a</code>, <code>b</code> and <code>c</code>.
- * <p/>
- * If iterator is between <code>b</code> and <code>c</code>
- * we can say that the event will be transferred to <code>c</code>
- * and the next one will be given to <code>a</code>.
- * <p/>
- * <pre>|a|b|c|      -->       |a|b|c|      -->       |a|b|c|</pre>
- * <pre>    ^     handle to c        ^   autorewind   ^</pre>
  *
  * @author Devgru &lt;devgru@mail.ru&gt;
  * @version 0.15
- */
-public final class RoundRobinBroadcaster<E extends Event>
-        implements HandlerBundle<Handler<E>, E>, Handler<E> {
+         */
+        public final class RoundRobinBroadcaster<E extends Event>
+                implements HandlerBundle<Handler<E>, E>, Handler<E> {
 
     private boolean empty = true;
+
+    @SuppressWarnings({"MismatchedQueryAndUpdateOfCollection"})
+    private final List<Handler<E>> handlers
+            = new LinkedList<Handler<E>>();
 
     /**
      * It works, just believe me.
      */
-    private final ListIterator<Handler<E>> storedIterator
-            = new LinkedList<Handler<E>>().listIterator();
+    private ListIterator<Handler<E>> storedIterator
+            = handlers.listIterator();
 
-    private final LinkedList<Handler<E>> handlersToRemove
+
+
+    private final List<Handler<E>> handlersToRemove
             = new LinkedList<Handler<E>>();
 
     /**
@@ -68,12 +64,6 @@ public final class RoundRobinBroadcaster<E extends Event>
         }
     }
 
-    private void rewindIfNeeded() {
-        if (!storedIterator.hasNext()) {
-            rewind();
-        }
-    }
-
     /**
      * If you add iterator is between <code>b</code> and <code>c</code>
      * we can say that the event will be transferred to <code>c</code>
@@ -91,14 +81,19 @@ public final class RoundRobinBroadcaster<E extends Event>
 
     public synchronized void removeHandler(Handler<E> h) {
         if (h == null) return;
+        if (!handlers.contains(h)) return;
         if (handlersToRemove.contains(h)) return;
         handlersToRemove.add(h);
     }
 
-    private synchronized void rewind() {
-        while (storedIterator.hasPrevious()) {
-            storedIterator.previous();
+    private void rewindIfNeeded() {
+        if (!storedIterator.hasNext()) {
+            rewind();
         }
+    }
+
+    private void rewind() {
+        storedIterator = handlers.listIterator();
     }
 
     public boolean isEmpty() {
