0aff77c307da5fbb07e9f414c3d0f5aa6c12d9b2
Devgru <java@devg.ru>
2009-03-17 23:15:06 +0300

    initial commit for downpushing

---
+++ base/src/ru/devg/dem/structures/dispatchers/inclass/PushesDown.java
@@ -0,0 +1,16 @@
+package ru.devg.dem.structures.dispatchers.inclass;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ * @author Devgru &lt;java@devg.ru&gt;
+ * @version 0.0
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface PushesDown {
+}
---
+++ base/src/ru/devg/dem/structures/dispatchers/inclass/binding/DownPusher.java
@@ -0,0 +1,8 @@
+package ru.devg.dem.structures.dispatchers.inclass.binding;
+
+/**
+ * @author Devgru &lt;java@devg.ru&gt;
+ * @version 0.0
+ */
+public class DownPusher {
+}
