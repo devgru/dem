f426c49f151ed97486b6d55aa039e81b4ff48fb7
Devgru <java@devg.ru>
2009-05-19 15:18:03 +0400

    added comments to Inclass dispatcher configuration

--- base/src/ru/devg/dem/inclass/Configuration.java
+++ base/src/ru/devg/dem/inclass/Configuration.java
@@ -1,9 +1,23 @@
 package ru.devg.dem.inclass;
 
 /**
+ * Inclass dispatcher configuration. May be passed to
+ * {@link InclassDispatcher#InclassDispatcher(Object, java.util.EnumSet)}.
+ *
  * @author Devgru &lt;java@devg.ru&gt;
  * @since 0.178
  */
 public enum Configuration {
-    broadcast, strictPrioritization
+
+    /**
+     * Every event passed to dispatcher can be handled by more than one methods/fields
+     */
+    broadcast,
+
+    /**
+     * Every class member with {@link Handles} annotation must have it's own priority,
+     * else {@link InclassDispatcher#InclassDispatcher(Object, java.util.EnumSet)} will throw a
+     * {@link ru.devg.dem.inclass.exceptions.ClassIsUnbindableException}
+     */
+    strictPrioritization
 }
