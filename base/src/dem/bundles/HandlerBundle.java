665d6f5563f81f568e7dc4cc7287809ac32a4e11
Devgru <java@devg.ru>
2009-06-27 21:09:56 +0400

    added some more docs

--- base/src/dem/bundles/HandlerBundle.java
+++ base/src/dem/bundles/HandlerBundle.java
@@ -6,6 +6,11 @@ import dem.quanta.Handler;
  * HandlerBundle is a way to use some strategies of bundling handlers.
  * If you want to use your own strategy - you just have
  * to implement this interface.
+ * <p/>
+ * There is no general contract about duplicate
+ * {@link Handler handlers} in one bundle, each implementation of {@link HandlerBundle}
+ * should specify its behavior in case of addition one {@link Handler}
+ * more than one time.
  *
  * @author Devgru &lt;java@devg.ru&gt;
  * @since 0.15
@@ -13,12 +18,22 @@ import dem.quanta.Handler;
 public interface HandlerBundle<H extends Handler> {
 
     /**
+     * Adds handler to {@link HandlerBundle bundle}.
+     * May do nothing if you try to add one handler
+     * to bundle more than one time.
+     *
      * @param handler handler to add
-     * @throws IllegalArgumentException may be thrown if handler is null
+     * @throws IllegalArgumentException <em>should be</em> thrown
+     *                                  if handler is <code>null</code>.
      */
     public void addHandler(H handler) throws IllegalArgumentException;
 
     /**
+     * Removes handler from {@link HandlerBundle bundle}.
+     * Should not throw any exception.
+     * Should do nothing if handler is <code>null</code> or if
+     * all its instances are already removed from bundle.
+     *
      * @param handler handler to add
      */
     public void removeHandler(H handler);
