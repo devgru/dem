2a0376e6f4c26d235fe8ee5c53e8425e40b53087
Devgru <java@devg.ru>
2009-06-28 13:48:46 +0400

    changes in ICD test

--- test/src/test/InclassDispatcherTest.java
+++ test/src/test/InclassDispatcherTest.java
@@ -1,17 +1,17 @@
 package test;
 
-import org.junit.Test;
 import dem.inclass.Handles;
 import dem.inclass.InclassDispatcher;
 import dem.inclass.exceptions.ClassIsUnbindableException;
 import dem.quanta.Handler;
 import dem.translating.TranslatorStrategy;
+import junit.framework.Assert;
+import org.junit.Test;
 import test.events.BaseEvent;
 import test.events.SecondLevelEvent1;
+import test.events.SecondLevelEvent2;
 import test.handlers.BaseHandler;
 import test.handlers.Collector;
-
-import junit.framework.Assert;
 
 /**
  * @author Devgru &lt;java@devg.ru&gt;
@@ -23,7 +23,6 @@ public class InclassDispatcherTest {
     public void simpleTest() throws ClassIsUnbindableException {
         WellFormedClass d = new WellFormedClass();
         Handler<BaseEvent> h = new InclassDispatcher<BaseEvent>(d);
-
         ClassIsUnbindableException exc = null;
         try {
             new InclassDispatcher<BaseEvent>(d, true);
@@ -36,7 +35,12 @@ public class InclassDispatcherTest {
         h.handle(new SecondLevelEvent1());
         h.handle(new BaseEvent());
 
-        assert c.getString().length() == 1;
+        Assert.assertTrue(c.getString().length() == 4);
+
+        Handler<BaseEvent> h2 = new InclassDispatcher<BaseEvent>(new Child());
+        System.out.println(h2);
+        h2.handle(new SecondLevelEvent1());
+        Assert.assertTrue(c.getString().length() == 8);
     }
 
     private final Collector c = new Collector();
@@ -46,12 +50,26 @@ public class InclassDispatcherTest {
         @Handles(SecondLevelEvent1.class)
         public Handler<SecondLevelEvent1> a = new BaseHandler<SecondLevelEvent1>(c, SecondLevelEvent1.class, "SLE1");
 
-        @Handles(value=SecondLevelEvent1.class, translator = TranslatorStrategy.class,priority = 0)
+        @Handles(value = SecondLevelEvent1.class, translator = TranslatorStrategy.class, priority = 0)
         public Handler<SecondLevelEvent1> b = new BaseHandler<SecondLevelEvent1>(c, SecondLevelEvent1.class, "SLE1");
 
     }
 
 
+    public class Parent {
+        @Handles(SecondLevelEvent1.class)
+        public Handler<SecondLevelEvent1> a = new BaseHandler<SecondLevelEvent1>(c, SecondLevelEvent1.class, "SLE1");
+
+    }
+
+    public final class Child extends Parent {
+        @Handles(SecondLevelEvent1.class)
+        public Handler<SecondLevelEvent2> a = new BaseHandler<SecondLevelEvent2>(c, SecondLevelEvent2.class, "SLE2");
+
+
+        public void x(){
+        }
+    }
 
 
 }
