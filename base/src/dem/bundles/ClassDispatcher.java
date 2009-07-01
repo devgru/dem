2decc4b5e5eca50fac75ac6a8c25696773cd6928
Devgru <java@devg.ru>
2009-07-01 17:48:23 +0400

    fixed class dispatcher

--- base/src/dem/bundles/ClassDispatcher.java
+++ base/src/dem/bundles/ClassDispatcher.java
@@ -31,12 +31,12 @@ public final class ClassDispatcher<E extends Event>
         handleIfPossible(event);
     }
 
-    public void addHandler(BoundedHandler<? extends E> handler) {
-        classTree.add(handler);
+    public boolean addHandler(BoundedHandler<? extends E> handler) {
+        return classTree.add(handler);
     }
 
-    public void removeHandler(BoundedHandler<? extends E> handler) {
-        classTree.remove(handler);
+    public boolean removeHandler(BoundedHandler<? extends E> handler) {
+        return classTree.remove(handler);
     }
 
     private static class ClassTree<E extends Event> extends BoundedHandler<E> {
@@ -56,15 +56,14 @@ public final class ClassDispatcher<E extends Event>
         }
 
         @SuppressWarnings("unchecked")
-        public <B extends E> void add(BoundedHandler<B> handler) {
+        public <B extends E> boolean add(BoundedHandler<B> handler) {
             Class<B> beta = handler.getBoundClass();
             if (beta == getBoundClass()) {
                 BoundedHandler<E> bh = (BoundedHandler<E>) handler;
-                if (target == null) {
+                boolean b = target == null;
+                if (b) {
                     target = bh;
-                } else {
-                    throw new RuntimeException();//todo specify
-                }
+                }return b;
             } else if (getBoundClass().isAssignableFrom(beta)) {
                 for (ClassTree<? extends E> subtree : subclasses.values()) {
                     Class<? extends E> alpha = subtree.getBoundClass();
@@ -72,8 +71,7 @@ public final class ClassDispatcher<E extends Event>
                     if (alpha.isAssignableFrom(beta)) {
                         //alpha is a parent - simply add alpha to beta subclasses
                         ClassTree<? super B> subtree2 = (ClassTree<? super B>) subtree;
-                        subtree2.add(handler);
-                        return;
+                        return subtree2.add(handler);
                     } else if (beta.isAssignableFrom(alpha)) {
                         //beta is a parent - remove alpha from list, add beta, add alpha to beta as a child
                         ClassTree<? extends B> subtree2 = (ClassTree<? extends B>) subtree;
@@ -81,20 +79,22 @@ public final class ClassDispatcher<E extends Event>
                         value.add(subtree2.target);
                         subclasses.remove(alpha);
                         subclasses.put(beta, value);
-                        return;
+                        return true;
                     }
                 }
                 subclasses.put(beta, new ClassTree<B>(handler));
+                return true;
             } else {
-                throw new RuntimeException();//todo specify
+                return false;
             }
 
         }
 
-        public <B extends E> void remove(BoundedHandler<B> handler) {
+        public <B extends E> boolean remove(BoundedHandler<B> handler) {
             Class<B> beta = handler.getBoundClass();
             if (beta == getBoundClass()) {
                 target = null;
+                return true;
             } else if (getBoundClass().isAssignableFrom(beta)) {
                 for (ClassTree<? extends E> subtree : subclasses.values()) {
                     Class<? extends E> alpha = subtree.getBoundClass();
@@ -102,16 +102,17 @@ public final class ClassDispatcher<E extends Event>
                     if (alpha == beta) {
                         //same classes
                         subtree.target = null;
-                        return;
+                        return true;
                     } else if (alpha.isAssignableFrom(beta)) {
                         //alpha is a parent - simply add alpha to beta subclasses
                         ClassTree<? super B> subtree2 = (ClassTree<? super B>) subtree;
                         subtree2.remove(handler);
-                        return;
+                        return true;
                     }
                 }
+                return false;
             } else {
-                throw new RuntimeException();//todo specify
+                return false;
             }
         }
 
@@ -130,7 +131,7 @@ public final class ClassDispatcher<E extends Event>
 
             for (ClassTree<? extends E> classTree : subclasses.values()) {
                 boolean result = classTree.handleIfPossible(event);
-                if (result) return true;
+                if (result) return tr   ue;
             }
 
             return target != null && target.handleIfPossible(event);
